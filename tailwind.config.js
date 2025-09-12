9820a3152ad888de904a4f102b37296f1f738410
reyhanhmdani <rey7dan7@gmail.com>
2025-09-12 14:30:46 +0700

    menambah halaman ponpes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,17 @@ module.exports = {
       colors: {
         primary: "#00ab66",
         secondary: "#1a365d",
-        biru: "#0099FF",
+        biru: "#2E5090",
+        biruMuda: "#00c3fe",
         toskaMuda: "#4ADE80",
         accent: "#f59e0b",
       },
       fontFamily: {
         heading: ["Inter", "sans-serif"],
         body: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        "gradient-custom": "radial-gradient(circle,rgba(0, 195, 254, 1) 0%, rgba(0, 171, 102, 1) 61%)",
       },
       fontSize: {
         xxs: [
